7be0a880f0bddac2387d905b573cae2cc1529485
liuxinghuao <liuxinghuao@163.com>
2016-02-14 16:36:37 +0800

    init

---
+++ obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,149 @@
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/hello-jni.c \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/jni.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavcodec/avcodec.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/errno.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/errno.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/errno.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/avutil.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/common.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/ctype.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/inttypes.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/limits.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/math.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/strings.h \
+ /cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/memory.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/attributes.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/mem.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/error.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/avutil.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/mathematics.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/rational.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/intfloat_readwrite.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/log.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/pixfmt.h \
+ /cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/avconfig.h
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavcodec/avcodec.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/errno.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/errno.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/errno.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/avutil.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/common.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/ctype.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/inttypes.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/math.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/attributes.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/mem.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/error.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/avutil.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/mathematics.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/rational.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/intfloat_readwrite.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/log.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/pixfmt.h:
+
+/cygdrive/d/android-ndk-r5b/samples/hello-jni/jni/ffmpeg/libavutil/avconfig.h:
